7c89d2555dcc97b70790a8bb612cada41dfa7fee
jwnbagas1945 <bagas_jwn@yahoo.co.id>
2018-10-10 00:05:55 +0700

    TUGAS 5 MUDAH2AN BENAR

---
+++ 6.sql
@@ -0,0 +1,12 @@
+/* create database and table */
+create database gusraicrud_db;
+
+use gusraicrud_db;
+
+CREATE TABLE `users` (
+	`id` int(11) NOT NULL auto_increment,
+	`name` varchar(100),
+	`email` varchar(100),
+	`mobile` varchar(15),
+	PRIMARY KEY (`id`)
+);
